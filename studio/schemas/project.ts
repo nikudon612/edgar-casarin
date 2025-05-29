cfcf287e20578f8c294f11718b2818b40eeb16d8
Nick Bechtel <87031638+nikudon612@users.noreply.github.com>
2025-05-29 16:46:56 -0400

    added list radio button for projects in be

--- studio/schemas/project.ts
+++ studio/schemas/project.ts
@@ -4,17 +4,29 @@ export default {
   type: 'document',
   title: 'Project',
   fields: [
-    { name: 'title', type: 'string' },
-    { name: 'slug', type: 'slug', options: { source: 'title', maxLength: 96 } },
-    { name: 'thumbnail', type: 'image', title: 'Cover Image or Video Thumbnail' },
-    { name: 'thumbnailVideo', type: 'file', title: 'Cover Video (optional)' },
-    { name: 'description', type: 'text' },
+    {name: 'title', type: 'string'},
+    {name: 'slug', type: 'slug', options: {source: 'title', maxLength: 96}},
+    {name: 'thumbnail', type: 'image', title: 'Cover Image or Video Thumbnail'},
+    {name: 'thumbnailVideo', type: 'file', title: 'Cover Video (optional)'},
+    {name: 'description', type: 'text'},
     {
       name: 'mediaGallery',
       type: 'array',
       title: 'Project Gallery',
-      of: [{ type: 'image' }, { type: 'file' }]
+      of: [{type: 'image'}, {type: 'file'}],
     },
-    { name: 'date', type: 'datetime' }
-  ]
-};
+    {name: 'date', type: 'datetime'},
+    {
+      name: 'row',
+      type: 'string',
+      title: 'Row',
+      options: {
+        list: [
+          {title: 'Row 1', value: 'row1'},
+          {title: 'Row 2', value: 'row2'},
+        ],
+        layout: 'radio', // or 'dropdown'
+      },
+    },
+  ],
+}
