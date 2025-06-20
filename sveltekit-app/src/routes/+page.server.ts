ca58fbe1d50506b7b1ce9e4c18997e8c69f34b66
Nick Bechtel <87031638+nikudon612@users.noreply.github.com>
2025-06-20 15:36:32 -0400

    updates to homepage seo, dynamic with cms and same as about page

--- sveltekit-app/src/routes/+page.server.ts
+++ sveltekit-app/src/routes/+page.server.ts
@@ -1,16 +1,32 @@
-import { allProjectsQuery, type Project } from '$lib/sanity/queries';
+import {
+	allProjectsQuery,
+	type Project,
+	aboutQuery as query,
+	type About
+} from '$lib/sanity/queries';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ locals }) => {
 	const { loadQuery } = locals;
-	const initial = await loadQuery<Project[]>(allProjectsQuery);
 
-	console.log('Projects from Sanity:', initial);
+	// Load projects
+	const projectInitial = await loadQuery<Project[]>(allProjectsQuery);
+
+	// Load about data
+	const aboutInitial = await loadQuery<About>(query);
+
+	console.log('Projects from Sanity:', projectInitial);
+	console.log('About from Sanity:', aboutInitial);
 
 	return {
 		query: allProjectsQuery,
 		options: {
-			initial
+			initial: {
+				data: {
+					projects: projectInitial.data,
+					about: aboutInitial.data
+				}
+			}
 		}
 	};
 };
