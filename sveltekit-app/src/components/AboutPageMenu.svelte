565df62fd49ee18b4d7f1b9cd02d57f06681eab4
Nick Bechtel <87031638+nikudon612@users.noreply.github.com>
2025-07-10 20:04:00 -0400

    responsive design for about page to mobile

--- sveltekit-app/src/components/AboutPageMenu.svelte
+++ sveltekit-app/src/components/AboutPageMenu.svelte
@@ -8,6 +8,17 @@
 		>design@edgarcasarin.xyz</a
 	>
 </div>
+<div class="mobile-menu">
+	<div class="mobile-menu-top">
+		<a href="/" style="text-decoration: none;" class="portfolio">Edgar Casarin</a>
+		<a href="mailto:@design@edgarcasarin.xyz" style="text-decoration: none;" class="info"
+			>design@edgarcasarin.xyz</a
+		>
+	</div>
+	<div class="mobile-menu-bottom">
+		<a href="/about" style="text-decoration: none; color:black" class="info">INFO</a>
+	</div>
+</div>
 
 <style>
 	.project-page-menu {
@@ -20,6 +31,9 @@
 		padding-right: 0.75rem;
 		overflow-y: hidden;
 	}
+	.mobile-menu {
+		display: none;
+	}
 	.portfolio {
 		font-size: 0.875rem;
 		padding-top: 0.75rem;
@@ -31,5 +45,46 @@
 		font-size: 0.875rem;
 		padding-bottom: 0.75rem;
 		color: #353535;
+	}
+
+	@media (max-width: 768px) {
+		.project-page-menu {
+			display: none;
+		}
+		.mobile-menu {
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			justify-content: space-between;
+			height: 100px;
+			padding-left: 0.5rem;
+			padding-right: 0.5rem;
+		}
+		.mobile-menu-top {
+			display: flex;
+			flex-direction: row;
+			align-items: center;
+			justify-content: space-between;
+			width: 100%;
+			padding-top: 0.5rem;
+		}
+		.mobile-menu-bottom {
+			display: flex;
+			flex-direction: row;
+			align-items: center;
+			justify-content: center;
+			width: 100%;
+			padding-bottom: 0.75rem;
+		}
+		.portfolio {
+			font-size: 1rem;
+			padding-top: 0rem;
+			letter-spacing: 2%;
+			line-height: 118%;
+		}
+		.info {
+			font-size: 1rem;
+			padding-bottom: 0rem;
+		}
 	}
 </style>
